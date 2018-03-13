f635ce0121e79d2a5dadd426f1e20b0947c4255e
Sby-EasyStuff <manuel.mancini@gmail.com>
2018-03-13 18:37:39 +0100

    Aggiunto lo user controller, i link alle user page. Iniziata user show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit, :update]
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'welcome#index'
 end
